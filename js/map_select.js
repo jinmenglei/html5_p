9b7050ceec89c42a67d877dc6064c1c1475db266
jinmenglei <229143434@qq.com>
2020-05-08 17:30:22 +0800

    20200507 完成自检界面

---
+++ js/map_select.js
@@ -0,0 +1,18 @@
+// var setting = {
+//     "posterWidth" : 280,  //幻灯片第一帧的宽度
+//     "posterHeight" : 280, //幻灯片第一帧的高度
+//     "scale" : 0.8,      //记录显示比例关系
+//     "speed" : 500,
+// }
+// var lifirst=$('#lifirst'),//第一个li
+//     lilast=$('#lilast'),//最后一个li
+//     all=$('.ull li'),//所有的li
+//     // prev,//
+//     next
+//     ali=all.slice(1),//出去第一帧的li
+//     num=ali.length/2,
+//     lf = 180/num ,   //li每次左移的值
+//     rightli=ali.slice(0,num),//去掉第一帧li的前一半
+//     leftli=ali.slice(num),//后一半li
+//     rw =setting.posterWidth,
+//     rh =setting.posterHeight;
