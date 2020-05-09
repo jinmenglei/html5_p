3b8d92a609ca2cba1b7ddeb4eb3480fd0216afe2
jinmenglei <229143434@qq.com>
2020-05-09 20:29:09 +0800

    20200509 静态结束

---
+++ js/ros_interface.js
@@ -0,0 +1,15 @@
+// console.log("ros start")
+// // Connecting to ROS
+// var ros = new ROSLIB.Ros({
+//     url : 'ws://192.168.0.106:9090'
+// });
+// //判断是否连接成功并输出相应的提示消息到web控制台
+// ros.on('connection', function() {
+//     console.log('Connected to websocket server.');
+// });
+// ros.on('error', function(error) {
+//     console.log('Error connecting to websocket server: ', error);
+// });
+// ros.on('close', function() {
+//     console.log('Connection to websocket server closed.');
+// });
